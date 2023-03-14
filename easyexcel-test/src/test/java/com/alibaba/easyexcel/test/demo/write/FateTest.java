574a035855fdee2bd2f96df3e35374a42ac5ff30
fwf <2584278672@qq.com>
2023-03-14 17:35:16 +0800

    第一次尝试提交

---
+++ easyexcel-test/src/test/java/com/alibaba/easyexcel/test/demo/write/FateTest.java
@@ -0,0 +1,11 @@
+package com.alibaba.easyexcel.test.demo.write;
+
+/**
+ * @author fate
+ * @version 1.0
+ * @date 2023/3/14 0014 17:34
+ * @className:FateTest
+ * @description
+ */
+public class FateTest {
+}
